4fa691c249377b20197467ec3c78edf4eb815a96
ArmandVulpe <145643483+ArmandVulpe@users.noreply.github.com>
2025-01-25 02:02:54 +0200

    Pagina signup

---
+++ frontend/pages/signuppage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupCard from '../components/signup';
+
+export default function Signup() {
+    return (
+        <section>
+            <div className="flex items-center justify-center min-h-screen">
+                <SignupCard />
+            </div>
+        </section>
+    );
+}
